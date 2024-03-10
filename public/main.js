01bc86fbf3addb22c621c9d173a66d4edfde33bd
bigpond4932 <hyt4932@gmail.com>
2024-03-10 23:09:45 +0900

    create: simple board project for studying nodeJS
    try to make a simple board wep app for studying

---
+++ public/main.js
@@ -0,0 +1,25 @@
+var targetDiv = document.querySelectorAll('.list-box');
+targetDiv.forEach((div) => {
+    div.addEventListener("click", function () {
+        console.log("div is pressed");
+        var id = this.id;
+        // redirect
+        window.location.href = `http://localhost:8080/articles/${id}`;
+    })
+    // fetch(`http://localhost:8080/articles/${id}`)
+    //     .then(response => { 
+    //         if (!response.ok) {
+    //             throw new Error('Network response was not ok ' + response.statusText);
+    //         }
+    //         return response
+    //     })
+    //     .then(data => {
+    //         console.log(data); // 가져온 데이터 처리
+    //         // 여기에서 가져온 데이터를 사용하여 무언가를 할 수 있습니다.
+    //         // 예: 동적으로 UI 업데이트
+    //     })
+    //     .catch(error => {
+    //         console.error('Fetch error:', error);
+    //         // 에러 처리 로직
+    //     });
+});
--- server.js
+++ server.js
@@ -1,32 +1,98 @@
 const express = require('express')
+// 예시: Node.js 코드에서 환경 변수 사용
 const app = express()
+const process = require('dotenv').config(); // dotenv 패키지를 사용해 .env 파일 로드
 
-app.use(express.static(__dirname + '/public'))
-
+app.use(express.static(__dirname + '/public')) // static 파일은 public 하위폴더에서 가져가라
+app.set('view engine', 'ejs') // ejs 사용을 위한 세팅
+app.use(express.urlencoded({ extended: true })); // form-data를 처리하기 위한 설정
+const { ObjectId } = require('mongodb') 
 const { MongoClient } = require('mongodb')
 
 let db
-const url = 'mongodb+srv://admin:fe1XvxhTQ0BXzc2g@mydb1.qyw6vbb.mongodb.net/?retryWrites=true&w=majority&appName=mydb1'
-new MongoClient(url).connect().then((client)=>{
-  console.log('DB연결성공')
-  db = client.db('forum')
-  app.listen(8080, () => {
-      console.log('http://localhost:8080 에서 서버 실행중')
-  })
-}).catch((err)=>{
-  console.log(err)
+// console.log(process);
+const url = process.parsed.DATABASE_URL;
+// console.log(url);
+new MongoClient(url).connect().then((client) => {
+    console.log('DB연결성공')
+    db = client.db('forum')
+    app.listen(8080, (req, res) => {
+        console.log('http://localhost:8080 에서 서버 실행중')
+    })
+}).catch((err) => {
+    console.log(err)
 })
 
+// app.listen(8080, () => {
+//     console.log('http://localhost:8080 에서 서버 실행중')
+// })
 
 app.get('/', (요청, 응답) => {
-//   응답.send('반갑다')
+    //   응답.send('반갑다')
     console.log(__dirname);
     응답.sendFile(__dirname + '/index.html')
-}) 
+})
+// 글작성/읽기 과제
+app.get('/write', (req, resp) => {
+    resp.render('write.ejs')
+})
 
-app.get('/news', (req, resp) =>{
+app.post('/write', async (req, resp) => { // async가 없으면 
+    var body = req.body;
+    var title = body.title;
+    var content = body.content;
+    console.log(title, content); // req-body parser가 필요하다.
+    if (title == '') {
+        resp.send('title을 입력해주세요');
+    } else {
+        try {
+            // 요청부분을 받기 -> 필요한 부분 몽고디비에 저장하기
+            await db.collection('post').insertOne({ title: title, content: content });
+            resp.redirect('/list');
+        } catch (e) {
+            console.log(e);
+            resp.status(500).send('error in server')
+        }
+    }
+})
+
+app.get('/list', async (req, resp) => { // async await는 왜 사용하는걸까?
+    var result = await db.collection('post').find().toArray(); // 기다려! JS는 참을성이 없다. 
+    // var firstTitle = result[0].title;
+    // resp.send(firstTitle);
+
+    resp.render('list.ejs', { articles: result }); // ejs템플릿 사용시 sendFile 대신 render로 응답
+})
+
+/**
+ * get 상세페이지 -> ObjectId를 이용한.
+ * TODO list 랜더링 시에 오브젝트 아이디를 각각의 글마다 붙여f놓는 것이 좀 필요할 듯
+ */
+app.get('/articles/:id', async (req, resp) => {
+    console.log(req.params);
+    var articleId = req.params.id;
+    if(articleId == ''){ // articles/는 매핑자체가 안된다.
+        console.log('articleId is \"\" ');
+        resp.redirect('/list');
+    }
+    // error_1.MongoInvalidArgumentError('Query filter must be a plain object or ObjectId');
+    // 매칭되는 document가 없으면 null 값이 나오는구나.
+    // var a, b, c = await db.collection('post').findOne({_id: new ObjectId(req.params.id)})
+    // console.log(a, b ,c); // undefined undefined 매칭객체
+    const article = await db.collection('post').findOne({_id: new ObjectId(articleId)});
+    if( article != null) {
+        // redirect 시에 데이터는 어떻게 담지?
+        resp.render('detail.ejs', {article: article}); // 뷰리졸버 덕분에 논리명만 입력하면 되는구나.
+    }else{
+        console.log('화면과 DB의 데이터가 불일치');
+        resp.redirect("/list");
+    }
+})
+
+app.get('/news', (req, resp) => {
     // 몽고DB 테스트
-    db.collection('post').insertOne({title : 'first commit'})
+    // db.collection('post').insertOne({title : 'first commit'})
+    // 성공
     resp.send('오늘 화창함');
 })
 
@@ -36,4 +102,46 @@ app.get('/about', (req, resp) => {
 
 app.get('/about2', (req, resp) => {
     resp.send('nodemon 적용 성공');
+})
+
+
+app.get('/time', (req, resp) => {
+    const currentDate = new Date();
+    const timestamp = currentDate.getTime(); // 1710030312629
+
+    function parseDay(day) {
+        switch (day) {
+            case 0:
+                return 'Sunday';
+            case 1:
+                return 'Monday';
+
+            case 2:
+                return 'Tuesday';
+
+            case 3:
+                return 'Wendsday';
+
+            case 4:
+                return 'Thursday';
+
+            case 5:
+                return 'Friday';
+
+            case 6:
+                return 'Saturday';
+        }
+    }
+
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth() + 1; // start with 0
+    const date = now.getDate();
+    const today = `${year}-${month}-${date}`;
+    const day = parseDay(now.getDay());
+    // returns a number representing the day of the week, starting with 0 for Sunday
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+
+    resp.send(`Today is ${today} ${day} and the time is ${hours}:${minutes}.`)
 })
